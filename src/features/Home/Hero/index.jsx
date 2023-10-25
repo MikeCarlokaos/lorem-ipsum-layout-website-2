641c885a81c4aa6bba3dd9688f8c940994d857a8
Mike Carlo Gonzaga <kaosvioge@gmail.com>
2023-10-25 18:09:40 +0800

    adjust hero section

--- src/features/Home/Hero/index.jsx
+++ src/features/Home/Hero/index.jsx
@@ -9,11 +9,11 @@ const Hero = () => {
       <div>
         <SlideCarousel slides={slides} />
       </div>
-      <div className="absolute z-10 inset-0 w-full h-full bg-gradient-to-br from-stone-200/10 via-stone-600/70 to-stone-800 blur"></div>
+      <div className="absolute z-10 inset-0 w-full h-full bg-gradient-to-tl from-stone-200/10 via-stone-600/70 to-stone-800"></div>
       <div className="absolute z-20 inset-0 w-full h-full flex items-center justify-center p-10 md:pt-20">
         <div className="text-white w-[98vw] h-96 flex flex-col items-center justify-center gap-10 p-5 md:w-96">
           <div className="w-full flex flex-col items-center md:w-[60vw]">
-            <div className="w-full p-10 border-t-8 border-l-8 border-white rounded-3xl">
+            <div className="w-full p-10 border-t-8 border-l-8 border-white rounded-tl-3xl">
               {/* border left and top with h3 on top right corner */}
             </div>
 
@@ -21,7 +21,7 @@ const Hero = () => {
               lorem ipsum
             </h1>
 
-            <div className="w-full p-10 border-b-8 border-r-8 border-white rounded-3xl">
+            <div className="w-full p-10 border-b-8 border-r-8 border-white rounded-br-3xl">
               {/* border right an bottom with */}
               <h3 className="text-2xl text-end ">upholstery</h3>
             </div>
