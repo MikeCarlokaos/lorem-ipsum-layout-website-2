8b2324030e4b78a974763ef28a1cd59c1b8959e5
Mike Carlo Gonzaga <kaosvioge@gmail.com>
2023-10-28 12:00:04 +0800

    update hero section

--- src/features/Home/Hero/index.jsx
+++ src/features/Home/Hero/index.jsx
@@ -1,9 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { HashLink } from "react-router-hash-link";
 import SlideCarousel from "../../../components/SlideCarousel";
 import slides from "../../../data/slides";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Hero = () => {
+  useEffect(() => {
+    AOS.init({ duration: 2000 });
+  }, []);
+
   return (
     <section id="home" className="relative w-full h-screen">
       <div>
@@ -13,21 +19,36 @@ const Hero = () => {
       <div className="absolute z-20 inset-0 w-full h-full flex items-center justify-center p-10 md:pt-20">
         <div className="text-white w-[98vw] h-96 flex flex-col items-center justify-center gap-10 p-5 md:w-96">
           <div className="w-full flex flex-col items-center md:w-[60vw]">
-            <div className="w-full p-10 border-t-8 border-l-8 border-white rounded-tl-3xl">
+            <div
+              data-aos="zoom-in-right"
+              className="w-full p-10 border-t-8 border-l-8 border-white rounded-tl-3xl"
+            >
               {/* border left and top with h3 on top right corner */}
             </div>
 
-            <h1 className="text-center text-5xl font-bold capitalize whitespace-nowrap md:text-6xl">
+            <h1
+              data-aos="zoom-in"
+              className="text-center text-5xl font-bold capitalize whitespace-nowrap md:text-6xl"
+            >
               lorem ipsum
             </h1>
 
-            <div className="w-full p-10 border-b-8 border-r-8 border-white rounded-br-3xl">
+            <div
+              data-aos="zoom-in-left"
+              className="w-full p-10 border-b-8 border-r-8 border-white rounded-br-3xl"
+            >
               {/* border right an bottom with */}
-              <h3 className="text-2xl text-end ">upholstery</h3>
+              <h3 data-aos="zoom-in-left" className="text-2xl text-end ">
+                upholstery
+              </h3>
             </div>
           </div>
-          <div className="flex items-center justify-center bg-stone-900 px-6 py-4 rounded-xl">
-            <HashLink smooth to="/#about">
+          <div data-aos="fade-up" className="flex items-center justify-center ">
+            <HashLink
+              smooth
+              to="/#about"
+              className=" w-full h-full px-6 py-4 bg-stone-900  rounded-xl ease-in-out duration-150 hover:bg-stone-700 active:bg-black"
+            >
               Get to know us
             </HashLink>
           </div>
